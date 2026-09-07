17e3c48142f89baba954ec2d44376671976723be
Oliver-Hendrik Hermlin <124428724+ohhermlin@users.noreply.github.com>
2026-09-07 10:04:16 +0000

    asdasd

---
+++ javascripti harjutused/ylesanne02.js
@@ -0,0 +1,21 @@
+// Nimi: Oliver
+// Kuupaev: 07.09.2026
+// Ulesande number: 01
+
+let arv = 4.2;
+let tekst1 = 'Will Code 4 food';
+let tekst2 = "123";
+let boolean1 = true;
+let massiiv = [1, 2, 3];
+let boolean2 = false;
+let tekst3 = 'true';
+let maaramata = undefined;
+
+console.log(typeof arv);
+console.log(typeof tekst1);
+console.log(typeof tekst2);
+console.log(typeof boolean1);
+console.log(typeof massiiv);
+console.log(typeof boolean2);
+console.log(typeof tekst3);
+console.log(typeof maaramata);
