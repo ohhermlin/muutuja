0f0278f73e42874d10427945596c05856afd44ae
Oliver-Hendrik Hermlin <124428724+ohhermlin@users.noreply.github.com>
2026-09-25 10:24:34 +0000

    temperatuur

---
+++ javascripti harjutus 7/ylesanne01.js
@@ -0,0 +1,4 @@
+const products = ["Õunad", "Piim", "Leib", "Juust", "Tomatid", "Kanafilee", "Muna", "Sibul", "Apelsinid", "Riis", "Jogurt", "Kartul", "Kalafilee", "Pasta", "Jogurtijook", "Porgandid", "Virsikud", "Pähklid", "Rosinad", "Kapsas", "Kreeka jogurt", "Veiseliha", "Banaanid", "Oliivid", "Mandlid", "Magus kartul", "Greibid"];
+let kummeesimest = products.slice(0, 10);
+let ilmaViimaseta = kummeesimest.slice(0, -1);
+console.log(ilmaviimaseta);
